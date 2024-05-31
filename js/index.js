9247ddf14ad6e76db570b6f277a84fc994aa6484
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-05-31 08:59:34 +0500

    yakunlandi

---
+++ js/index.js
@@ -0,0 +1,163 @@
+// 1-masala
+//let a = +prompt("a sonini kiriting");
+//let P = 4 * a;
+//console.log(P);
+
+//2-masala
+//let a = +prompt("kvardat tomonini kiriting");
+//let S = a * a;
+//console.log(S);
+
+//3-masala
+//let a = +prompt("tortburchakni a tomonini kiriting");
+//let b = +prompt("tortburchakni b tomonini kiriting");
+//let S = a * b;
+//let P = 2 * (a+b);
+//console.log(S);
+//console.log(P);
+
+
+//4-masala
+//let d = +prompt("aylananing diametrini kiriting");
+//let L = Math.PI*d;
+//console.log(L);
+
+//5-masala
+//let a = +prompt("kubning a tomonini kiriting");
+//let V = a * a * a;
+//let S = 6*(a * a);
+//console.log(V);
+//console.log(S);
+
+//6-masala
+//let a = +prompt("Paralelepepidning a tomonini kiriting");
+//let b = +prompt("Paralelepepidning b tomonini kiriting");
+//let c = +prompt("Paralelepepidning c tomonini kiriting");
+//let V = a*b*c;
+//let S = 2 * (a*b +b*c+a*c);
+//console.log(V);
+//console.log(S);
+
+//7-masala
+//let R = +prompt("doiraning radiusi R ni kiriting");
+//let L = 2 * Math.PI*R;
+//let S = Math.PI*(R*R);
+//console.log(L);
+//console.log(S);
+
+//8-masala
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//let n = (a+b)/2;
+//console.log(n);
+
+//13-masala
+//let R1 = +prompt("birinchi aylana radiusini kiriting");
+//let R2 = +prompt("ikkinchi aylana radiusini kiriting");
+//let S1 = Math.PI*R1;
+//let S2 = Math.PI*R2;
+//let S3 = Math.PI*(R1-R2);
+//console.log(S1);
+//console.log(S2);
+//console.log(S3);
+
+//32-masala
+//let Tf = +prompt("Temperaturani kiriting");
+//let Tc = (Tf-32)*(5/9);
+//console.log(Tc);
+
+//integer1
+//let L = +prompt("L santimetrni kiriting");
+//let m = L/100;
+//console.log(m);
+
+//INTEGER2
+//let kg = +prompt("KLOGRAMNI KIRITING kiriting");
+//let t = kg/1000;
+//console.log(t);
+
+//integer3
+//let bayt = +prompt("baytlarni kiriting");
+//let kb = bayt/1024;
+//console.log("kb");
+
+//integer4
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//let c = a/b;
+//console.log(c);
+
+//integer5
+//let a = +prompt("a sonini kiriting");
+//let b = +prompt("b sonini kiriting");
+//let c = a%b;
+//console.log(c);
+
+//integer6
+//let on = +prompt("ikki honali son kiriting");
+//let onlik = on/10;
+//let birlik = on%10;
+//console.log(onlik);
+//console.log(birlik);
+
+//integer7
+//let on = +prompt("ikki honali son kiriting");
+//let a = on/10;
+//let b = on%10;
+//let y = a+b;
+//console.log(y);
+
+//integer8
+// let on = +prompt("ikki honali son kiriting");
+// let a = on/10;
+// let b = on%10;
+// let c = b*10;
+// let y = c+a;
+// console.log(y);
+
+
+//integer9
+// let yuz = +prompt("uch honali son kiriting");
+// let a = yuz / 100;
+// console.log(a);
+
+//integer10
+// let yuz = +prompt("uch honali son kiriting");
+// let b= yuz%10;
+// let on = yuz%100/10;
+// console.log(b);
+// console.log(on);
+
+
+//integer11
+// let yuz = +prompt("uch honali son kiriting");
+// let b= yuz%10;
+// let on = yuz%100/10;
+// let uch = yuz/100;
+// let y = b+on+uch;
+// console.log(y);
+
+
+//integer12
+// let yuz = +prompt("uch honali son kiriting");
+// let b= yuz%10;
+// let on = yuz%100/10;
+// let uch = yuz/100;
+// let s =b*100;
+// let d = on*10;
+// let y = s+d+s;
+// console.log(y);
+
+//integer13
+
+// let yuz = +prompt("uch honali son kiriting");
+// let b= yuz%10;
+// let on = yuz%100/10;
+// let uch = yuz/100;
+// let s =b*100;
+// let d = on*10;
+// let y = s+d+s;
+// console.log(uch);
+
+
+//integer6
